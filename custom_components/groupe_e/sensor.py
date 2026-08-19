9d57b77fd89691e390538c494ce31f29498587dd
Jocelyn Thode <jocelyn@thode.email>
2026-08-19 19:33:30 +0200

    Use total_increasing for monthly and yearly

--- custom_components/groupe_e/sensor.py
+++ custom_components/groupe_e/sensor.py
@@ -29,7 +29,7 @@ class GroupeEEnergySensor(CoordinatorEntity, SensorEntity):
         self._attr_name = "Groupe-E Energy Consumption"
         self._attr_unique_id = f"{coordinator.premise}_energy"
         self._attr_device_class = SensorDeviceClass.ENERGY
-        self._attr_state_class = SensorStateClass.TOTAL
+        self._attr_state_class = SensorStateClass.TOTAL_INCREASING
         self._attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
 
     @property
@@ -80,7 +80,7 @@ class GroupeEMonthlyEnergySensor(CoordinatorEntity, SensorEntity):
         self._attr_name = "Groupe-E Monthly Energy Consumption"
         self._attr_unique_id = f"{coordinator.premise}_monthly_energy"
         self._attr_device_class = SensorDeviceClass.ENERGY
-        self._attr_state_class = SensorStateClass.TOTAL
+        self._attr_state_class = SensorStateClass.TOTAL_INCREASING
         self._attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
 
     @property
